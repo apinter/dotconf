e21b3604563d0640411188d14638028fb152d7f1
Attila Pinter <apinter.it@gmail.com>
2017-06-02 22:09:15 +0700

    Updated dotfiles and installer for Arch

---
+++ Arch-installer/AUR_installer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+yaourt -S i3-gaps-git i3blocks netdiscover playerctl skypeforlinux-bin terminus-font-ttf ttf-font-awesome adapta-gtk-theme artwork-maia bumblebee-status-git chrome-gnome-shell-git gtkd maia-gtk-theme maia-icon-theme maia-wallpaper menda-maia-icon-theme nerd-fonts-git netdiscover oh-my-zsh-git oh-my-zsh-powerline-theme-git package-query packer-color plasma5-themes-maia pop-icon-theme-git qutebrowser-git ranger-git rofi sddm-maia-theme ttf-share-gf wordgrinder xbanish zsh-packer-color 
--- Arch-installer/i3wm_apps_AUR.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-yaourt -S i3-gaps-git i3blocks netdiscover playerctl skypeforlinux-bin terminus-font-ttf ttf-font-awesome 
