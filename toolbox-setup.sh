2a637507fa1e8538b17f1476f16475d4dbfa0004
Attila Pinter <adathor@opensuse.org>
2022-05-29 22:42:03 +0700

    checking if Brave and VSCode repos are added already or not

--- toolbox-setup.sh
+++ toolbox-setup.sh
@@ -2,19 +2,31 @@
 
 set -e
 
-## Add repo for VSCode
-sudo zypper in -y openssl curl
-sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
-sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-#md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/#zypp/repos.d/vscode.repo'
+code_repo=$(sudo zypper lr | grep code | awk '{print $3}')
+brave_repo=$(sudo zypper lr | grep brave | awk '{print $3}')
 
-## Add repo for Brave
-sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
-sudo zypper addrepo --refresh https://brave-browser-rpm-release.s3.brave.com/x86_64/ brave-browser
+sudo zypper in -y openssl curl 
+
+if [ "$code_repo" ]; then
+	echo "VSCode repo is already present"
+else
+	## Add repo for VSCode
+	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc && \
+	sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-#md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
+fi 
+
+if [ "$brave_repo" ]; then
+	echo "Brave repo is already present"
+else
+	## Add repo for Brave
+	sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc && \
+	sudo zypper addrepo --refresh https://brave-browser-rpm-release.s3.brave.com/x86_64/ brave-browser
+fi
 
 ## Install packages
 sudo zypper ref
 sudo zypper in -y -t pattern devel_C_C++ devel_basis devel_python3
-sudo zypper in -y brave-browser code vim python38 python38-devel python38-pip kitty alacritty fish zsh ripgrep opi ffmpeg mlocate sshpass cryfs wireguard-tools pwgen htop kubernetes1.23-client k9s go bat git openssh-common openssh-clients
+sudo zypper in -y tmux ranger fontawesome-fonts font-manager fira-code-fonts font-manager brave-browser code vim python38 python38-devel python38-pip kitty alacritty fish zsh ripgrep opi ffmpeg mlocate sshpass cryfs wireguard-tools pwgen htop kubernetes1.23-client k9s go bat git openssh-common openssh-clients nodejs-common npm xdg-desktop-portal-gnome xdg-desktop-portal-wlr  
 
 ## Install GUI app dependencies 
 sudo zypper install adwaita-icon-theme xorg-x11-fonts libX11-xcb1 gsettings-desktop-schemas gdk-pixbuf-query-loaders
@@ -25,7 +37,7 @@ pip install ansible ansible-vault ara google-auth pymongo
 
 ## Install logo-ls
 cd $HOME/Project/git/ && curl -LO https://github.com/Yash-Handa/logo-ls/releases/download/v1.3.7/logo-ls_amd64.rpm
-sudo zypper in -y logo-ls_amd64.rpm
+sudo zypper in $HOME/Project/git/logo-ls_amd64.rpm
 
 ## Install OMF
 curl https://raw.githubusercontent.com/oh-my-fish/oh-my-fish/master/bin/install | fish
