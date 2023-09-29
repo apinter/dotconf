6b60cde72a55821d5a965a6a1eb3f18806dee692
Attila Pinter <adathor@opensuse.org>
2023-09-29 12:48:50 +0700

    update configuration with flatpak auto update unit, time, added remote Home mount
    Signed-off-by: Attila Pinter <adathor@opensuse.org>

--- nix/bryxina/configuration.nix
+++ nix/bryxina/configuration.nix
@@ -1,5 +1,11 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
 { config, pkgs, callPackage, ... }:
 {
+  #nixpkgs.config.pulseaudio = true;
+
   security.rtkit.enable = true;
   services.pipewire = {
     enable = true;
@@ -37,17 +43,45 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      ./systemd-user/flatpak-auto-update.nix
     ];
 
   # Use the systemd-boot EFI boot loader.
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
-  networking.hostName = "bryxina";
-  networking.networkmanager.enable = true;
+  networking.hostName = "bryxina"; # Define your hostname.
+  # Pick only one of the below networking options.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  networking.networkmanager.enable = true;  # Easiest to use and most distros use this by default.
 
   # Set your time zone.
   time.timeZone = "Asia/Jakarta";
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  #   useXkbConfig = true; # use xkbOptions in tty.
+  # };
+
+  # Enable the X11 windowing system.
+  # services.xserver.enable = true;
+
+
+  
+
+  # Configure keymap in X11
+  # services.xserver.layout = "us";
+  # services.xserver.xkbOptions = {
+  #   "eurosign:e";
+  #   "caps:escape" # map caps to escape.
+  # };
 
   # Enable CUPS to print documents.
   # services.printing.enable = true;
@@ -64,12 +98,22 @@
   services.printing.allowFrom = [ "all" ]; # this gives access to anyone on the interface you might want to limit it see the official documentation
   services.printing.defaultShared = true; # If you want
 
+  # Enable sound.
+  #sound.enable = true;
+  #hardware.pulseaudio.enable = true;
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
 
   # Define a user account. Don't forget to set a password with ‘passwd’.
 users.users.bryxina = {
   initialPassword = "pw123";
   isNormalUser = true;
-  extraGroups = [ "wheel" ];
+  extraGroups = [ "wheel" "scanner" "lp" ]; # Enable ‘sudo’ for the user.
+  #   packages = with pkgs; [
+  #     firefox
+  #     thunderbird
+  #   ];
 };
 
 users.users.apinter = {
@@ -115,10 +159,49 @@ users.groups.devops.gid = 5000;
    su
 ];
 
-# Enable the OpenSSH daemon.
-services.openssh.enable = true;
+  systemd.package = pkgs.systemd.override { withSelinux = true; };
 
-networking.firewall.enable = false;
-system.stateVersion = "23.05";
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+ services.openssh.enable = true;
+  # Scanner 
+ hardware.sane.enable = true;
+ hardware.sane.extraBackends = [ pkgs.sane-backends ];
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ 25565 ];
+  # networking.firewall.allowedUDPPorts = [ 25565 ];
+  # Or disable the firewall altogether.
+ networking.firewall.enable = false;
+ fileSystems."/home/bryxina/Common" = {
+    device = "172.168.1.3:/shirayuki/Common";
+    fsType = "nfs";
+ };
+
+ fileSystems."/home/bryxina/Reno" = {
+    device = "172.168.1.3:/shirayuki/Home/skye";
+    fsType = "nfs";
+ };
+  
+  # Copy the NixOS configuration file and link it from the resulting system
+  # (/run/current-system/configuration.nix). This is useful in case you
+  # accidentally delete configuration.nix.
+  # system.copySystemConfiguration = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "23.05"; # Did you read the comment?
 
 }
