7b794418a755ac99444484b5bd6d3ab93f9678c3
Attila Pinter <adathor@opensuse.org>
2024-02-17 07:11:22 +0700

    update: cleanup, change nvim theme

--- nix/brenda/garbagecollect.nix
+++
@@ -1,9 +0,0 @@
-{ config, lib, pkgs, ... }:
-
-{
-  nix.gc.automatic = true;
-  nix.gc.persistent = true;
-  nix.gc.dates = "weekly";
-  nix.gc.options = "--delete-older-than 30d";
-  nix.gc.randomizedDelaySec = "30m";
-}
--- nvim/lua/plugins/color.lua
+++ nvim/lua/plugins/color.lua
@@ -1,56 +1,56 @@
 return {
-  -- add gruvbox
-  -- { "ellisonleao/gruvbox.nvim" },
+	-- add gruvbox
+	-- { "ellisonleao/gruvbox.nvim" },
 
-  -- Configure LazyVim to load gruvbox
-  --{
-  --  "LazyVim/LazyVim",
-  --  opts = {
-  --    colorscheme = "tokyonight-night",
-  --  },
-  -- },
-  { "nordtheme/vim", name = "nord" },
-  {
-    "LazyVim/LazyVim",
-    opts = {
-      colorscheme = "nord",
-    },
-  },
-  {
-    "catppuccin/nvim",
-    lazy = true,
-    name = "catppuccin",
-    flavour = "mocha",
-    opts = {
-      integrations = {
-        alpha = true,
-        cmp = true,
-        flash = true,
-        gitsigns = true,
-        illuminate = true,
-        indent_blankline = { enabled = true },
-        lsp_trouble = true,
-        mason = true,
-        mini = true,
-        native_lsp = {
-          enabled = true,
-          underlines = {
-            errors = { "undercurl" },
-            hints = { "undercurl" },
-            warnings = { "undercurl" },
-            information = { "undercurl" },
-          },
-        },
-        navic = { enabled = true, custom_bg = "lualine" },
-        neotest = true,
-        noice = true,
-        notify = true,
-        neotree = true,
-        semantic_tokens = true,
-        telescope = true,
-        treesitter = true,
-        which_key = true,
-      },
-    },
-  },
+	-- Configure LazyVim to load gruvbox
+	--{
+	--  "LazyVim/LazyVim",
+	--  opts = {
+	--    colorscheme = "tokyonight-night",
+	--  },
+	-- },
+	{ "nordtheme/vim", name = "nord" },
+	{
+		"LazyVim/LazyVim",
+		opts = {
+			colorscheme = "gruvbox",
+		},
+	},
+	{
+		"catppuccin/nvim",
+		lazy = true,
+		name = "catppuccin",
+		flavour = "mocha",
+		opts = {
+			integrations = {
+				alpha = true,
+				cmp = true,
+				flash = true,
+				gitsigns = true,
+				illuminate = true,
+				indent_blankline = { enabled = true },
+				lsp_trouble = true,
+				mason = true,
+				mini = true,
+				native_lsp = {
+					enabled = true,
+					underlines = {
+						errors = { "undercurl" },
+						hints = { "undercurl" },
+						warnings = { "undercurl" },
+						information = { "undercurl" },
+					},
+				},
+				navic = { enabled = true, custom_bg = "lualine" },
+				neotest = true,
+				noice = true,
+				notify = true,
+				neotree = true,
+				semantic_tokens = true,
+				telescope = true,
+				treesitter = true,
+				which_key = true,
+			},
+		},
+	},
 }
