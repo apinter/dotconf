dca1925a74871b70d60625569914a7b95750ff35
Attila Pinter <adathor@opensuse.org>
2024-12-05 02:05:21 +0700

    chore: update pyhton version
    Signed-off-by: Attila Pinter <adathor@opensuse.org>

--- toolbox-setup.sh
+++ toolbox-setup.sh
@@ -8,27 +8,27 @@ edge_repo=$(sudo zypper lr | grep edge | awk '{print $3}')
 sudo zypper in openssl curl
 
 if [ "$code_repo" ]; then
-	echo "VSCode repo is already present"
+  echo "VSCode repo is already present"
 else
-	## Add repo for VSCode
-	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
-		sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-#md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
+  ## Add repo for VSCode
+  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+    sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ntype=rpm-#md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/zypp/repos.d/vscode.repo'
 fi
 
 if [ "$edge_repo" ]; then
-	echo "MSEdge repo is already present"
+  echo "MSEdge repo is already present"
 else
-	## Add repo for VSCode
-	sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
-		sudo zypper ar https://packages.microsoft.com/yumrepos/edge microsoft-edge-beta
+  ## Add repo for VSCode
+  sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc &&
+    sudo zypper ar https://packages.microsoft.com/yumrepos/edge microsoft-edge-beta
 fi
 
 if [ "$brave_repo" ]; then
-	echo "Brave repo is already present"
+  echo "Brave repo is already present"
 else
-	## Add repo for Brave
-	sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc &&
-		sudo zypper addrepo --refresh https://brave-browser-rpm-release.s3.brave.com/x86_64/ brave-browser
+  ## Add repo for Brave
+  sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc &&
+    sudo zypper addrepo --refresh https://brave-browser-rpm-release.s3.brave.com/x86_64/ brave-browser
 fi
 
 ## Install Nix
@@ -41,33 +41,33 @@ fi
 sudo zypper ref
 sudo zypper in -t pattern devel_C_C++ devel_basis devel_python3
 sudo zypper in Mesa microsoft-edge-beta \
-	tmux ranger pinentry-gnome3 \
-	fontawesome-fonts font-manager \
-	fira-code-fonts font-manager \
-	brave-browser code-insiders vim \
-	python311 python311-devel python311-pip \
-	kitty alacritty fish zsh ripgrep opi \
-	ffmpeg mlocate sshpass cryfs \
-	wireguard-tools pwgen htop neovim \
-	command-not-found kubernetes1.24-client \
-	kubectx go git openssh-common \
-	openssh-clients nodejs-common npm \
-	xdg-desktop-portal-gnome xdg-desktop-portal-wlr \
-	xdg-dbus-proxy xdg-desktop-portal-gtk \
-	xdg-desktop-portal-wlr xdg-user-dirs \
-	xdg-user-dirs-gtk pinentry bind-utils \
-	noisetorch qrencode unzip zip \
-	apache2-utils wine jq \
-	fd fzf gnome-keyring zellij \
-	zoxide yazi difftastic \
-	broot dust eza yq 
+  tmux ranger pinentry-gnome3 \
+  fontawesome-fonts font-manager \
+  fira-code-fonts font-manager \
+  brave-browser code-insiders vim \
+  python313 python313-devel python313-pip \
+  kitty alacritty fish zsh ripgrep opi \
+  ffmpeg mlocate sshpass cryfs \
+  wireguard-tools pwgen htop neovim \
+  command-not-found kubernetes1.24-client \
+  kubectx go git openssh-common \
+  openssh-clients nodejs-common npm \
+  xdg-desktop-portal-gnome xdg-desktop-portal-wlr \
+  xdg-dbus-proxy xdg-desktop-portal-gtk \
+  xdg-desktop-portal-wlr xdg-user-dirs \
+  xdg-user-dirs-gtk pinentry bind-utils \
+  noisetorch qrencode unzip zip \
+  apache2-utils wine jq \
+  fd fzf gnome-keyring zellij \
+  zoxide yazi difftastic \
+  broot dust eza yq
 
 echo -n "" | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install gh just \
-	k9s bat
+  k9s bat
 
 /home/linuxbrew/.linuxbrew/bin/brew tap hashicorp/tap &&
-	home/linuxbrew/.linuxbrew/bin/brew install hashicorp/tap/terraform
+  home/linuxbrew/.linuxbrew/bin/brew install hashicorp/tap/terraform
 
 ## Install GUI app dependencies
 sudo zypper install adwaita-icon-theme xorg-x11-fonts libX11-xcb1 gsettings-desktop-schemas gdk-pixbuf-query-loaders
