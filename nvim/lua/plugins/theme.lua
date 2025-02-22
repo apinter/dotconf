6956e8c3d7a404d6cb949f9cce8be2b872755338
Attila Pinter <adathor@opensuse.org>
2025-02-23 00:15:05 +0700

    chore: added themes

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,4 +1,20 @@
 return {
+  -- 'projekt0n/github-nvim-theme',
+  -- name = 'github-theme',
+  -- lazy = false, 
+  -- priority = 1000,
+  -- config = function()
+  --   require('github-theme').setup({
+  --   })
+  --
+  --   vim.cmd('colorscheme github_dark')
+  -- end,
+
+  -- "shaunsingh/nord.nvim",
+  -- config = function ()
+  --    vim.cmd("colorscheme nord")
+  -- end
+
     "ramojus/mellifluous.nvim",
     -- version = "v0.*", -- uncomment for stable config (some features might be missed if/when v1 comes out)
     config = function()
