b4f3093f354207e65be20743bcee5611443da31c
Attila Pinter <adathor@protonmail.com>
2025-07-19 00:42:59 +0700

    fix: search in hidden files
    Signed-off-by: Attila Pinter <adathor@protonmail.com>

--- nvim/lua/plugins/telescope.lua
+++ nvim/lua/plugins/telescope.lua
@@ -20,6 +20,21 @@ return {
         end,
       },
     },
-
+    opts = {
+      defaults = {
+        file_ignore_patterns = { "%.git/" },
+        vimgrep_arguments = {
+          "rg",
+          "--color=never",
+          "--no-heading",
+          "--with-filename",
+          "--line-number",
+          "--column",
+          "--smart-case",
+          "--hidden"
+        },
+        find_command = { 'rg', '--hidden', '--color=never', '--no-heading', '--with-filename', '--line-number', '--column', '--smart-case', '-g', '!{.git,.svn,.hg}' }
+      }
+    }
   }
 }
