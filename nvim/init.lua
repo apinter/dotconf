f18517bef62f0ae3d3c5b7b86fcda1a595ff6f93
Attila Pinter <adathor@opensuse.org>
2025-08-11 10:41:12 +0700

    chore: set crlf
    Signed-off-by: Attila Pinter <adathor@opensuse.org>

--- nvim/init.lua
+++ nvim/init.lua
@@ -9,6 +9,9 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 vim.opt.autoindent = false
+vim.opt.winborder = "rounded"
+vim.opt.fileformats = { "unix", "dos" }
+vim.opt.fileformat = "unix"
 
 vim.opt.wrap = false
 vim.opt.swapfile = false
